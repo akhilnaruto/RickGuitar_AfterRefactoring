c9fae8516626cf58aedfac1818a06e18ae8c142f
Akhil <akhil.munna02@gmail.com>
2015-11-16 21:19:46 +0530

    empty solution

---
+++ RickGuitar_AfterRefactoring/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickGuitar_AfterRefactoring
+{
+    public class Class1
+    {
+    }
+}
