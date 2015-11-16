16bbefd90fbee77fa1b9800996acbe01e964ea5f
Akhil <akhil.munna02@gmail.com>
2015-11-16 22:10:19 +0530

    Adding enum files

---
+++ RickGuitar_AfterRefactoring/Wood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RickGuitar_AfterRefactoring
+{
+    public enum Wood
+    {
+        INDIAN_ROSEWOOD, BRAZILIAN_ROSEWOOD, MAHOGANY, MAPLE,
+        COCOBOLO, CEDAR, ADIRONDACK, ALDER, SITKA
+    }
+}
